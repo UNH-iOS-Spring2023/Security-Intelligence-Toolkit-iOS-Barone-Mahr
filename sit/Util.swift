16690997a641baa6ab65303414dfb37be91510ef
Andrew Mahr <63127691+acompsec1@users.noreply.github.com>
2023-03-19 20:58:36 -0400

    Initial HistoryScanCardView work to display History results #7

--- sit/Util.swift
+++ sit/Util.swift
@@ -146,3 +146,12 @@ struct Util {
     }
     
 }
+
+// https://stackoverflow.com/questions/35700281/date-format-in-swift
+extension Date {
+   func getFormattedDate(format: String) -> String {
+        let dateformat = DateFormatter()
+        dateformat.dateFormat = format
+        return dateformat.string(from: self)
+    }
+}
