1c05fdc9eae9397d9a368b033166b44009cbcea8
Charles_ <charliebarone2@icloud.com>
2023-04-02 03:57:03 -0400

    Implemented backend functions for ShodanAPI queries #24

--- sit/UI/ShodanView.swift
+++ sit/UI/ShodanView.swift
@@ -17,8 +17,59 @@ struct ShodanView: View {
                 .ignoresSafeArea()
             
             VStack {
-                Text("Shodan")
-                    .foregroundColor(.white)
+                Button(action: {
+                    let api = ShodanAPI()
+
+                    api.testAPIConnection { result in
+                        switch result {
+                        case .success(let isConnected):
+                            if isConnected {
+                                print("API connection successful")
+                            } else {
+                                print("API connection failed")
+                            }
+                        case .failure(let error):
+                            print("Error: \(error.localizedDescription)")
+                        }
+                    }
+
+                    api.getHostInformation(ipAddress: "8.8.8.8") { result in
+                        switch result {
+                        case .success(let hostInfo):
+                            print("Host information for 8.8.8.8: \(hostInfo)")
+                        case .failure(let error):
+                            print("Error: \(error.localizedDescription)")
+                        }
+                    }
+                    
+                    api.getPublicIPAddress { result in
+                        switch result {
+                        case .success(let publicIP):
+                            print("Your public IP address is: \(publicIP)")
+                        case .failure(let error):
+                            print("Error: \(error.localizedDescription)")
+                        }
+                    }
+                    
+                    api.search(query: "webcam") { result in
+                        switch result {
+                        case .success(let searchResults):
+                            print("Search results: \(searchResults)")
+                        case .failure(let error):
+                            print("Error: \(error.localizedDescription)")
+                        }
+                    }
+
+                }, label: {
+                    Text("Test Shodan")
+                        .font(.callout)
+                        .bold()
+                })
+                .frame(maxWidth: .infinity, minHeight: 44)
+                .background(CustomColors.pink?.suColor)
+                .foregroundColor(.white)
+                .cornerRadius(8)
+                .padding(.horizontal)
             }
         }
     }
